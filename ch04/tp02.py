8392e04ab5210dee5cc04f045445587653305e9d
Frédéric GAURAT <fgaurat@gmail.com>
2024-08-26 14:36:07 +0200

    loop

---
+++ ch04/tp02.py
@@ -0,0 +1,14 @@
+l = ["Valeur 01","Valeur 02","Valeur 03","Valeur 04"]
+
+for item in l:
+    print(item)
+
+
+for i in range(3,15,2):
+    print(i)
+
+
+# 0 Valeur 01 
+# 1 Valeur 02
+# 2 Valeur 03
+# 3 Valeur 04
