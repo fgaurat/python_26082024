12c5f55a836a0aeb98abd1584cdc9118d6bb41b0
Frédéric GAURAT <fgaurat@gmail.com>
2024-08-26 14:13:55 +0200

    add guess number

--- ch04/tp01.py
+++ ch04/tp01.py
@@ -1,2 +1,13 @@
-value = input('Saisir une valeur :')
-print(value)
+to_find = 12
+found = False
+
+while not found: 
+    value = int(input('Saisir une valeur :'))
+
+    if value>to_find:
+        print("c'est moins")
+    elif value<to_find:
+        print("c'est plus")
+    else:
+        print("ok")
+        found = True
