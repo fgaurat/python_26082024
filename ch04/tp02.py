88161c54b203d245d71da0d6458ddf85897d12dd
Frédéric GAURAT <fgaurat@gmail.com>
2024-08-26 15:03:09 +0200

    loop else

--- ch04/tp02.py
+++ ch04/tp02.py
@@ -12,3 +12,35 @@ for i in range(3,15,2):
 # 1 Valeur 02
 # 2 Valeur 03
 # 3 Valeur 04
+
+for i in range(len(l)):
+    print(str(i)+" "+l[i])
+
+
+r = range(5) # [0,1,2,...]
+l = list(r)
+print(l)
+
+
+print(50*'-')
+
+
+l = [2,56,3,7,90,54]
+to_found = 777
+
+# for i in l:
+#     if i == to_found:
+#         print("ok "+str(i))
+#         continue
+#     print(i)
+
+
+for i in l:
+    print(i)
+    if i == to_found:
+        print("ok "+str(i))
+        break
+else: # s'éxécute si la boucle n'a pas été interrompue
+    print("pas de break")
+
+
