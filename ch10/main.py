23414fd5013538fb17b4fa467b0185db886e54c6
Frédéric GAURAT <fgaurat@gmail.com>
2024-08-29 10:38:49 +0200

    use dao

---
+++ ch10/main.py
@@ -0,0 +1,11 @@
+from CustomerDAO import CustomerDAO
+def main():
+    dao = CustomerDAO('customers.db')
+
+    customers = dao.find_all()
+
+    for customer in customers:
+        print(customer)
+
+if __name__ == '__main__':
+    main()
